377f259e88bc84a3bfaca635c7fe0191dcb164f9
julasweta <julasweta@ukr.net>
2023-10-14 21:34:14 +0300

    add: cors

--- src/app.js
+++ src/app.js
@@ -3,13 +3,26 @@ import mongoose from "mongoose";
 import {PostsRouter} from "./routes/posts.router.js";
 import {WorksRouter} from "./routes/works.router.js";
 import {configs} from "./configs/config.js"; 
-const cors = require('cors'); 
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(cors()); 
+app.all('*', (req, res, next) => {
+  const {origin, Origin, referer, Referer} = req.headers;
+  const allowOrigin = origin || Origin || referer || Referer || '*';
+  res.header("Access-Control-Allow-Origin", allowOrigin);
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("X-Powered-By", 'Express');
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use("/posts", PostsRouter);
 app.use("/works", WorksRouter);
